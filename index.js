0ee4cf0fc30b42a4af148553a0ece1b98be6f613
MatteoPas93 <pascucci-matteooutlook.it>
2024-01-09 19:36:11 +0100

    aggiunto commenti

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const pexelsApi = "https://api.pexels.com/v1/search?query=maldive"
+const pexelsApi = "https://api.pexels.com/v1/search?query=italy"
+
+
+// !NAV SECTION
+
+const nav = document.querySelector(".container-nav")
+
+nav.innerHTML += `<div class="title">
+<h1>Album Fotografico</h1>
+</div>
+<div class="icon">
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+</svg>
+</div>`
 
 
 
+// !CONTAINER CARDS
 const fetchImg = () => (
     fetch(pexelsApi, {
         headers: {
