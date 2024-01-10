b76c420ad4dc7fab9913a275cf9526bf6647802c
MatteoPas93 <pascucci-matteooutlook.it>
2024-01-10 18:53:24 +0100

    module

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
+
 const pexelsApi = "https://api.pexels.com/v1/search?query=italy";
 const pexelsApi2 =
   "https://api.pexels.com/v1/search/?page=1&per_page=35&query=italy";
+
 
 // !NAV SECTION
 
